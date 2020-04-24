a7a90acd12cd10d1c4f3fa4870595f673cc5575c
Pascal Berger <pascal.berger@gmail.com>
2020-04-25 00:30:12 +0200

    Add empty addin

---
+++ src/Cake.Issues.DupFinder.Tests/DupFinderIssuesProviderTests.cs
@@ -0,0 +1,43 @@
+﻿namespace Cake.Issues.DupFinder.Tests
+{
+    using System;
+    using System.Linq;
+    using Cake.Issues.Testing;
+    using Cake.Testing;
+    using Shouldly;
+    using Xunit;
+
+    public sealed class DupFinderIssuesProviderTests
+    {
+        public sealed class TheCtor
+        {
+            [Fact]
+            public void Should_Throw_If_Log_Is_Null()
+            {
+                // Given / When
+                var result = Record.Exception(() =>
+                    new DupFinderIssuesProvider(
+                        null,
+                        new DupFinderIssuesSettings("Foo".ToByteArray())));
+
+                // Then
+                result.IsArgumentNullException("log");
+            }
+
+            [Fact]
+            public void Should_Throw_If_IssueProviderSettings_Are_Null()
+            {
+                // Given / When
+                var result = Record.Exception(() => new DupFinderIssuesProvider(new FakeLog(), null));
+
+                // Then
+                result.IsArgumentNullException("issueProviderSettings");
+            }
+        }
+
+        public sealed class TheReadIssuesMethod
+        {
+            //// TODO Add test cases for business logic
+        }
+    }
+}
---
+++ src/Cake.Issues.DupFinder/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fd97d151-5199-4366-bd4e-120ee29d2ff4")]
+
+[assembly: CLSCompliant(true)]
+[assembly: InternalsVisibleTo("Cake.Issues.DupFinder.Tests")]
