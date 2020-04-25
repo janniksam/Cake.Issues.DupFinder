c0d1048a8019024f9c6c117110acb5dc96a9651f
Jannik Kowalzik <hugo934753@web.de>
2020-04-25 04:26:38 +0200

    Inital logic to read the issues

--- src/Cake.Issues.DupFinder.Tests/DupFinderIssuesProviderTests.cs
+++ src/Cake.Issues.DupFinder.Tests/DupFinderIssuesProviderTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace Cake.Issues.DupFinder.Tests
 {
-    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Linq;
     using Cake.Issues.Testing;
     using Cake.Testing;
@@ -37,7 +38,55 @@
 
         public sealed class TheReadIssuesMethod
         {
-            //// TODO Add test cases for business logic
+            private const string ExpectedIssueMessage =
+                "Possible duplicate detected (cost 100).\r\nThe following fragments were found that might be duplicates:\r\n`Src\\Bar.cs` (Line 17 to 233)";
+
+            [Fact]
+            public void Should_Read_Issue_Correct()
+            {
+                // Given
+                var fixture = new DupFinderIssuesProviderFixture("DupFinder.xml");
+
+                // When
+                var issues = fixture.ReadIssues().ToList();
+
+                // Then
+                issues.Count.ShouldBe(4);
+
+                var issueToVerify = issues.Single(p => p.FilePath() == "Src/Foo.cs" && p.Line == 16);
+                IssueChecker.Check(
+                    issueToVerify,
+                    IssueBuilder.NewIssue(
+                            ExpectedIssueMessage,
+                            "Cake.Issues.DupFinder.DupFinderIssuesProvider",
+                            "DupFinder")
+                        .InFile(@"Src\Foo.cs", 16)
+                        .OfRule("dupFinder")
+                        .WithPriority(IssuePriority.Warning)
+                        .Create());
+            }
+
+            [Fact(Timeout = 50)]
+            public void ShouldReadBigFileWith1000DuplicatesInUnder50ms()
+            {
+                // Given
+                var fixture = new DupFinderIssuesProviderFixture("DupFinder1000Duplicates.xml");
+
+                IList<IIssue> issues = new List<IIssue>();
+
+                // When
+                var sw = Stopwatch.StartNew();
+                for (var i = 0; i < 10; i++)
+                {
+                    issues = fixture.ReadIssues().ToList();
+                }
+
+                sw.Stop();
+
+                // Then
+                issues.Count.ShouldBe(2885);
+                sw.ElapsedMilliseconds.ShouldBeLessThan(500);
+            }
         }
     }
 }
--- src/Cake.Issues.DupFinder/DupFinderIssuesProvider.cs
+++ src/Cake.Issues.DupFinder/DupFinderIssuesProvider.cs
@@ -1,6 +1,10 @@
 ﻿namespace Cake.Issues.DupFinder
 {
     using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Linq;
     using Cake.Core.Diagnostics;
 
     /// <summary>
@@ -26,9 +30,135 @@
         {
             var result = new List<IIssue>();
 
-            //// TODO Implement parsing of log file
+            var logDocument = XDocument.Parse(this.IssueProviderSettings.LogFileContent.ToStringUsingEncoding());
+            foreach (var duplicate in logDocument.Descendants("Duplicate"))
+            {
+                if (!TryGetCost(duplicate, out var cost))
+                {
+                    continue;
+                }
+
+                // Gets all fragments from a duplicate
+                var fragments = GetDuplicateFragements(duplicate);
+                if (fragments.Count < 2)
+                {
+                    continue;
+                }
+
+                foreach (var fragment in fragments)
+                {
+                    var builder = new StringBuilder();
+                    builder.AppendLine($"Possible duplicate detected (cost {cost}).");
+                    builder.AppendLine("The following fragments were found that might be duplicates:");
+                    foreach (var possibleDuplicateFragment in fragments.Where(innerDuplicate => !innerDuplicate.Equals(fragment)))
+                    {
+                        builder.Append($"`{possibleDuplicateFragment.File}` (Line {possibleDuplicateFragment.LineStart} to {possibleDuplicateFragment.LineEnd})");
+                    }
+
+                    result.Add(
+                        IssueBuilder
+                            .NewIssue(builder.ToString(), this)
+                            .InFile(fragment.File, fragment.LineStart)
+                            .OfRule("dupFinder")
+                            .WithPriority(IssuePriority.Warning)
+                            .Create());
+                }
+            }
 
             return result;
+        }
+
+        private static IReadOnlyCollection<DuplicateFragment> GetDuplicateFragements(XContainer duplicate)
+        {
+            var items = new List<DuplicateFragment>();
+            foreach (var issue in duplicate.Descendants("Fragment"))
+            {
+                if (!TryGetFile(issue, out var file))
+                {
+                    continue;
+                }
+
+                if (!TryGetLine(issue, out var lineStart, out var lineEnd))
+                {
+                    continue;
+                }
+
+                items.Add(new DuplicateFragment(file, lineStart, lineEnd));
+            }
+
+            return items;
+        }
+
+        private static bool TryGetCost(XElement duplicate, out int cost)
+        {
+            cost = 0;
+
+            var costAttr = duplicate.Attribute("Cost");
+            if (costAttr == null)
+            {
+                return false;
+            }
+
+            var costValue = costAttr.Value;
+            if (string.IsNullOrWhiteSpace(costValue))
+            {
+                return false;
+            }
+
+            cost = int.Parse(costValue, CultureInfo.InvariantCulture);
+            return true;
+        }
+
+        private static bool TryGetFile(XContainer fragment, out string fileName)
+        {
+            fileName = string.Empty;
+
+            var fileAttr = fragment.Element("FileName");
+            if (fileAttr == null)
+            {
+                return false;
+            }
+
+            fileName = fileAttr.Value;
+            return !string.IsNullOrWhiteSpace(fileName);
+        }
+
+        private static bool TryGetLine(XContainer fragment, out int lineStart, out int lineEnd)
+        {
+            lineStart = -1;
+            lineEnd = -1;
+
+            var lineRangeElement = fragment.Element("LineRange");
+            var lineStartAttr = lineRangeElement?.Attribute("Start");
+            var lineEndAttr = lineRangeElement?.Attribute("End");
+
+            var lineStartValue = lineStartAttr?.Value;
+            var lineEndValue = lineEndAttr?.Value;
+            if (string.IsNullOrWhiteSpace(lineStartValue) ||
+                string.IsNullOrWhiteSpace(lineEndValue))
+            {
+                return false;
+            }
+
+            lineStart = int.Parse(lineStartValue, CultureInfo.InvariantCulture);
+            lineEnd = int.Parse(lineEndValue, CultureInfo.InvariantCulture);
+            return true;
+        }
+
+        private class DuplicateFragment
+        {
+            internal DuplicateFragment(string file, int lineStart, int lineEnd)
+            {
+                this.File = file; 
+                this.LineStart = lineStart;
+                this.LineEnd = lineEnd;
+            }
+
+            internal string File { get; }
+
+            internal int LineStart { get; }
+
+            internal int LineEnd { get; }
         }
     }
 }
